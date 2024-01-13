40d933aaae996763650433f73ca683ab81a123e4
c0nfleis <bj.maldonado-miranda@panther.io>
2024-01-12 22:06:59 -0500

    Adding function comments and cleaning up function

--- src/web/helpers.ts
+++ src/web/helpers.ts
@@ -1,15 +1,18 @@
-export function getRootPathFromRunnablePath(basePath: string) {
-    if (basePath.includes("/u/")) {
-        if (basePath.endsWith("/u")) {
-            basePath = basePath.split("/u")[0];
-        } else {
-            basePath = basePath.split("/u/")[0];
-        }
-    } else if (basePath.includes("/f")) {
-        if (basePath.endsWith("/f")) {
-            basePath = basePath.split("/f")[0];
-        } else {
-            basePath = basePath.split("/f/")[0];
+/*
+    This function takes in the path of a runnable and parses out the root path of the windmill project
+    Also accounts for multiple instances of "/u*" or "/f*" in the path
+
+    Examples:
+    "/windmill/under/windmill-src/f/testing/bar.py" => "/windmill/under/windmill-src"
+    "/windmill/free/windmill-src/u/ryan/bar.py" => "/windmill/free/windmill-src"
+*/
+export function getRootPathFromRunnablePath(basePath: string): string {
+    const dirs = ["/u/", "/f/"];
+
+    for (const dir of dirs) {
+        if (basePath.includes(dir)) {
+            basePath = basePath.endsWith(dir) ? basePath.split(dir)[0] : basePath.split(`${dir}`)[0];
+            break;
         }
     }
 
